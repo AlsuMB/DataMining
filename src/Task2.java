c144a8d26e3ea9c2734e1f071676e4ffd452586c
AlsuMB <65676823+AlsuMB@users.noreply.github.com>
2020-05-23 09:49:53 +0300

    task3 and task2

--- src/Task2.java
+++ src/Task2.java
@@ -12,6 +12,11 @@ public class Task2 {
         PrintWriter out = new PrintWriter(
                 new FileWriter(".\\src\\out.xlsx"));
 
+        HashMap<String, String> hashMap1 = new HashMap<>();
+
+        PrintWriter out1 = new PrintWriter(
+                new FileWriter(".\\src\\allBasket.txt"));
+        // считаем сколько раз купили продукт
         while (sc.hasNext()) {
 
             String[] product = sc.nextLine().split(";");
@@ -21,11 +26,29 @@ public class Task2 {
             } else {
                 hashMap.put(product[0], 1);
             }
+
+            // какие продукты входят в корзину №3
+            if (hashMap1.containsKey(product[1])) {
+                hashMap1.put(product[1], hashMap1.get(product[1]) + " " + product[0]);
+            } else {
+                hashMap1.put(product[1], product[0]);
+            }
         }
 
+        // список сколько раз купили продукт (всего, среди всех покупок) в корзине  №3
+        for (Map.Entry<String, String > a : hashMap1.entrySet()) {
+            String[] str = a.getValue().split(" ");
+            for (int i = 0; i < str.length; i++) {
+                out1.print(hashMap.get(str[i]) + " ");
+            }
+            out1.print("\n");
+
+        }
 
         Map<String, Integer> map = hashMap;
         List list = new ArrayList(map.entrySet());
+
+        // сортировка для облегченного просмотра
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
@@ -33,11 +56,14 @@ public class Task2 {
             }
         });
 
+        // записываем в документ, потом составляем график из данных
         for (Object a : list) {
             String[] str = a.toString().split("=");
             out.println(str[0] + str[1]);
 
         }
         out.close();
+        out1.flush();
+
     }
 }
